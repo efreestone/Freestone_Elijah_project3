63eec267e8f1ded356c39e9c25e5cc048b46bca5
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2012-10-10 22:04:23 -0600

    Delete date working correctly. Small tweaks to css.

--- js/main.js
+++ js/main.js
@@ -140,7 +140,7 @@ window.addEventListener("DOMContentLoaded", function() {
     	deleteLink.href = "#";
     	deleteLink.key = key;
     	var deleteText = "Delete Date";
-    	//deleteLink.addEventListener("click", deleteItem);
+    	deleteLink.addEventListener("click", deleteItem);
     	deleteLink.innerHTML = deleteText;
     	linksLi.appendChild(deleteLink);
 	    
@@ -154,6 +154,7 @@ window.addEventListener("DOMContentLoaded", function() {
 	    
 	    //Show the form
 	    toggleControls("off");
+	    
 	    
 	    //Populate the form fields with current localStorage values.
 	    $("events").value = item.events[1];
@@ -182,6 +183,17 @@ window.addEventListener("DOMContentLoaded", function() {
 	    editSubmit.addEventListener("click", validate);
 	    editSubmit.key = this.key;
 	    
+    };
+    
+    function deleteItem() {
+	    var ask = confirm("Are you sure you want to delete this date?");
+	    if(ask) {
+		    localStorage.removeItem(this.key);
+		    alert("You have successfully deleted the date!")
+		    window.location.reload();
+	    }else{
+		    alert("Date was NOT deleted.")
+	    }
     };
     
     function clearData() {
